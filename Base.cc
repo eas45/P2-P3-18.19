200c59319ae1902bdca2fbbb5583e954a82deeb4
Esteban Antón <eas45@alu.ua.es>
2019-05-10 16:34:21 +0200

    Clase Base sin terminar

--- Base.cc
+++ Base.cc
@@ -1,2 +1,96 @@
 // 48788593G - Esteban Antón Sellés
 
+#include "Base.h"
+
+Base::Base(string name)
+{
+  this->name = name;
+}
+
+/* Devuelve el contenedor buscado por su ID
+  * Return::Container
+  * Throw::
+    - ERR_CONTAINER_ID
+*/
+Container Base::getContainer(unsigned int id) const
+{
+  int pos = searchContainer(id);
+
+  if (pos == -1)
+  {
+    throw ERR_CONTAINER_ID;
+  }
+
+  return containers[pos];
+}
+
+/* Devuelve el puntero a la nave buscada por su nombre
+  * Return::Ship*
+  * Throw::
+    - ERR_SHIP_NAME
+*/
+Ship* Base::getShip(string name) const
+{
+  int pos = searchShip(name);
+
+  if (pos == -1)
+  {
+    throw ERR_SHIP_NAME;
+  }
+
+  return ships[pos];
+}
+
+/* Devuelve la posición del contenedor en el vector 'containers' buscado por el ID
+  * Return::int
+    - (posición) -> Si encuentra el contenedor
+    - (-1) -> Si no encuentra el contenedor
+*/
+int Base::searchContainer(unsigned int id) const
+{
+  int pos = -1;
+
+  for (unsigned int i = 0; i < containers.size() && pos == -1; i++)
+  {
+    if (containers[i].getId() == id)
+    {
+      pos = i;
+    }
+  }
+
+  return pos;
+}
+
+/* Devuelve la posición de la nave en el vector 'ships' buscada por el nombre
+  * Return::int
+    - (posición) -> Si encuentra la nave
+    - (-1) -> Si no encuentra la nave
+*/
+int Base::searchShip(string name) const
+{
+  int pos = -1;
+
+  for (unsigned int i = 0; i < ships.size() && pos == -1; i++)
+  {
+    if (ships[i]->getName() == name)
+    {
+      pos = i;
+    }
+  }
+
+  return pos;
+}
+
+/* Añade un contenedor a la Base
+  * Return::bool
+    - True -> Se ha podido añadir
+    - False -> No se ha podido añadir
+*/
+bool Base::addContainer(unsigned int id, unsigned int value)
+{
+  bool added = false;
+
+  
+
+  return added;
+}
--- Base.h
+++ Base.h
@@ -1,2 +1,32 @@
 // 48788593G - Esteban Antón Sellés
 
+#ifndef _BASE_
+#define _BASE_
+
+#include "Ship.h"
+
+class Base
+{
+
+  protected:
+    string name;
+    vector<Ship*> ships;
+    vector<Container> containers;
+
+  private:  // Métodos y funciones propios
+  
+
+  public:
+    Base(string name);
+    string getName() const { return name; }
+    unsigned int getNumContainers() const { return containers.size(); }
+    unsigned int getNumShips() const { return ships.size(); }
+    Container getContainer(unsigned int id) const;
+    Ship* getShip(string name) const;
+    int searchContainer(unsigned int id) const;
+    int searchShip(string name) const;
+    bool addContainer(unsigned int id = 0, unsigned int value = 0);
+    bool removeContainer(unsigned int id = 0);
+};
+
+#endif
